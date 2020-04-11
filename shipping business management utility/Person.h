b7cfd58bfe9f2a376762271bf1aa84c672f3f737
emre <emre.kyd11@gmail.com>
2020-04-11 16:21:38 +0300

    define atribute Person base class

---
+++ shipping business management utility/Person.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person // base Class
+{
+private:
+    string name;
+    string surname;
+public:
+    Person(/* args */);
+    ~Person();
+};
+
+Person::Person(/* args */)
+{
+}
+
+Person::~Person()
+{
+}
