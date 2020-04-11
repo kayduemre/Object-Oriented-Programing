2e10a2465b91e3aa087e31bca515550bf77c28d5
emre <emre.kyd11@gmail.com>
2020-04-11 16:22:17 +0300

    define vehicle class

---
+++ shipping business management utility/Vehicle.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Vehicle       //composition class
+{
+private:
+    string type;;
+    int capacity;
+public:
+    Vehicle(/* args */);
+    ~Vehicle();
+};
+
+Vehicle::Vehicle(/* args */)
+{
+}
+
+Vehicle::~Vehicle()
+{
+}
