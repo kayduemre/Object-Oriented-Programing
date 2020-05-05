7d13b58332007f203b21d1f98bfd02a3fd161fd9
emre <emre.kyd11@gmail.com>
2020-05-05 21:04:27 +0300

    delete OwnerShip class

--- shipping business management utility/Owner.h
+++ shipping business management utility/Owner.h
@@ -1,20 +1,45 @@
+/*
+    Emre KAYDU
+    150160552
+
+*/
+
+#ifndef OWNER_H
+#define OWNER_H
 
 #include <iostream>
 #include <string>
 #include "Person.h"
-#include "OwnerShip.h"
 
 using namespace std;
+
 class Owner: public Person   //derived Class
 {
 private:
-    OwnerShip _ownership;     //composition
+    
+    static int _percentage;
 public:
+    static int _ownership_number;
     Owner();
     Owner(string, string);
+    int getNumber(){return _ownership_number;};
+    int getPercentage(){return _percentage;};
     ~Owner();
 };
-Owner::Owner(string name, string surname):Person(name, surname){}
+int Owner::_ownership_number = 0;
+int Owner::_percentage = 0;
+Owner::Owner(){ _ownership_number = 0;}
+
+Owner::Owner(string name, string surname):Person(name, surname)
+{
+    _ownership_number +=1;
+    _percentage = 100/_ownership_number;
+}
+
+
+
 Owner::~Owner()
 {
+    
 }
+#endif
