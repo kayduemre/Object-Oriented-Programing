d56094fd89a3f81fe1b6694cb11e8409149f48f6
emre <emre.kyd11@gmail.com>
2020-05-05 21:00:53 +0300

    adding static object atribute

--- shipping business management utility/Courier.h
+++ shipping business management utility/Courier.h
@@ -1,5 +1,10 @@
+/*
+    Emre KAYDU
+    150160552
 
-
+*/
+#include <iostream>
+#include <string>
 #include "Person.h"
 #include "Vehicle.h"
 
@@ -8,13 +13,17 @@ using namespace std;
 class Courier: public Person  // derived class
 {
 private:
-    Vehicle vehicle;
+    Vehicle *vehicle;
+    int _order;
 public:
     Courier *next;
     static int _index;
 
     Courier(){ next = NULL;};
     Courier(string, string, string);
+
+    Vehicle getVehicle(){return *vehicle;};
+    int getOrder()const{return _order;};
     ~Courier();
 };
 int Courier::_index = 0;
@@ -22,6 +31,8 @@ Courier::Courier(string name, string surname, string Vehicle_type):Person(name, 
 {
     next = NULL;
     _index++;
+    _order = _index;
+    vehicle = new Vehicle(Vehicle_type);
 }
 
 Courier::~Courier()
