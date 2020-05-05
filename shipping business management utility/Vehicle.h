0d78820924bd538749e406b9b6edfd19390084e5
emre <emre.kyd11@gmail.com>
2020-05-05 21:08:43 +0300

    create vehicle class

--- shipping business management utility/Vehicle.h
+++ shipping business management utility/Vehicle.h
@@ -1,3 +1,11 @@
+/*
+    Emre KAYDU
+    150160552
+
+*/
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
 #include <iostream>
 #include <string>
 
@@ -5,17 +13,45 @@ using namespace std;
 class Vehicle       //composition class
 {
 private:
-    string type;;
-    int capacity;
+    string _type;;
+    int _capacity;
 public:
-    Vehicle(/* args */);
+    Vehicle();
+    Vehicle(string);
+
+    string gettype(){return _type;};
+    int getcapacity(){return _capacity;};
     ~Vehicle();
 };
-
-Vehicle::Vehicle(/* args */)
+Vehicle::Vehicle()
 {
+    _type = "";
+    _capacity = 0;
+}
+Vehicle::Vehicle(string type)
+{
+    if (type.compare("car") == 0)
+    {
+        _type.assign("car");
+        _capacity = 200;
+        
+    }else if (type.compare("motorcycle") == 0)
+    {
+        _type.assign("motorcycle");
+        _capacity = 35;
+    }
+    else if (type.compare("bicycle") == 0)
+    {
+        _type.assign("bicycle");
+        _capacity = 10;
+    }
+    else
+    {
+        cout<<"fail"<<endl;
+    }
 }
 
 Vehicle::~Vehicle()
 {
 }
+#endif
